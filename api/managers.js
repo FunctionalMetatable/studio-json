c8989dc876eaa61a42d5e19b39334c5c8e67d93b
FunctionalMetatable <>
2021-06-02 10:42:48 +0000

    stop the duplicates

--- api/managers.js
+++ api/managers.js
@@ -5,7 +5,7 @@ module.exports = (req, res) => {
   fetch(`https://scratch.mit.edu/site-api/users/owners-in/${req.query.studio}/${req.query.page}/`).then(r => r.text()).then(html => {
     const { window, document } = parseHTML(html)
 
-    let all = Array.from(document.querySelectorAll('.avatar.thumb.vertical a'))
+    let all = Array.from(document.querySelectorAll('.avatar.thumb.vertical .info.title a'))
 
     let array = []
 
